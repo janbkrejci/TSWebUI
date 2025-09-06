1230d168e530c32403d7f22333ad18c1b7702975
janbkrejci <janbkrejci@gmail.com>
2025-09-06 13:07:53 +0200

    Přidání šablon pro DATATABLE a LOGIN, včetně stylů a skriptů pro interakci

---
+++ src/stories/DATATABLE.stories.js
@@ -0,0 +1,53 @@
+import { fn } from 'storybook/test';
+import template from './DATATABLE.html?raw';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+export default {
+  title: 'TSWebUI/DATATABLE',
+  tags: ['autodocs'],
+  render: ({ label, ...args }) => {
+    const theme = args.dark ? 'dark' : 'light';
+    return template.replace(/\{\{label\}\}/g, label)
+                   .replace(/\{\{theme\}\}/g, theme)
+                   .replace(/\{\{args\}\}/g, JSON.stringify(args, null, 2))
+                   .replace(/\{\{variant\}\}/g, args.variant ? `variant="${args.variant}"` : '');
+  },
+  argTypes: {
+    //backgroundColor: { control: 'color' },
+    label: { control: 'text' },
+    dark: { control: 'boolean' },
+    variant: {
+      control: { type: 'select' },
+      options: ['', 'primary', 'success', 'warning', 'danger', 'neutral', ],
+    },
+    // bgintensity: {
+    //   control: { type: 'select' },
+    //   options: ['50', '100', '200', '300', '400', '500', '600', '700', '800', '900', '950'],
+    // },
+    // fgcolor: {
+    //   control: { type: 'select' },
+    //   options: ['primary', 'success', 'warning', 'danger', 'neutral', ],
+    // },
+    // fgintensity: {
+    //   control: { type: 'select' },
+    //   options: ['50', '100', '200', '300', '400', '500', '600', '700', '800', '900', '950'],
+    // },
+  },
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  //args: { onClick: fn() },
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const DARK = {
+  args: {
+    dark: true,
+    label: "DARK",
+  },
+};
+
+export const LIGHT = {
+  args: {
+    dark: false,
+    label: "LIGHT",
+  },
+};
