6f21d70b2b699dbb7a2cf486eadbe6047e1eed61
janbkrejci <janbkrejci@gmail.com>
2025-10-12 09:30:42 +0200

    new record moved to web component

--- src/ts-table.js
+++ src/ts-table.js
@@ -279,3 +279,36 @@ class TSTablePager extends HTMLElement {
 }
 
 customElements.define('ts-table-pager', TSTablePager);
+
+class TSCreateRecordButton extends HTMLElement {
+    constructor() {
+        super();
+        this.innerHTML = `
+            <style>
+                .create-record-btn {
+                    display: inline-block;
+                }
+            </style>
+            <sl-tooltip content="Nový záznam">
+                <sl-button class="create-record-btn">
+                    <sl-icon name="plus-lg"></sl-icon>
+                </sl-button>
+            </sl-tooltip>
+        `;
+    }
+
+    connectedCallback() {
+        this.setupEventListeners();
+    }
+
+    setupEventListeners() {
+        const button = this.querySelector('.create-record-btn');
+        if (button) {
+            button.addEventListener('click', () => {
+                this.dispatchEvent(new CustomEvent('new-record', { detail: {} }));
+            });
+        }
+    }
+}
+
+customElements.define('ts-create-record-button', TSCreateRecordButton);
