97e173d0e2971f4b9f4ab2dff00362b00ecda988
janbkrejci <janbkrejci@gmail.com>
2025-10-12 08:08:00 +0200

    update

---
+++ src/stories/TSTABLE.stories.js
@@ -0,0 +1,49 @@
+import { fn } from 'storybook/test';
+import template from './TSTABLE.html?raw';
+import './TSTABLE.css';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+export default {
+  title: 'TSWebUI/TSTABLE',
+  tags: ['autodocs'],
+  render: (args) => {
+    const theme = args.dark ? 'dark' : 'light';
+    return template.replace(/\{\{theme\}\}/g, theme);
+  },
+  argTypes: {
+    //backgroundColor: { control: 'color' },
+    dark: { control: 'boolean' },
+    // bgintensity: {
+    //   control: { type: 'select' },
+    //   options: ['50', '100', '200', '300', '400', '500', '600', '700', '800', '900', '950'],
+    // },
+    // fgcolor: {
+    //   control: { type: 'select' },
+    //   options: ['primary', 'success', 'warning', 'danger', 'neutral', ],
+    // },
+    // fgintensity: {
+    //   control: { type: 'select' },
+    //   options: ['50', '100', '200', '300', '400', '500', '600', '700', '800', '900', '950'],
+    // },
+  },
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  //args: { onClick: fn() },
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+// export const DARK = {
+//   args: {
+//     dark: true,
+//   },
+// };
+
+// export const LIGHT = {
+//   args: {
+//     dark: false,
+//   },
+// };
+export const DEFAULT = {
+  args: {
+    dark: false,
+  },
+};
