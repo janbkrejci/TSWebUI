4de2b5f38e55ab48780741c82b0f81fe0ef2ce7c
janbkrejci <janbkrejci@gmail.com>
2026-01-24 17:11:47 +0100

    update

--- shadcn/next.config.ts
+++ shadcn/next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   // Enable static HTML export (replaces `next export`)
   output: 'export',
   /* config options here */
+  basePath: '/shadcn',
+  turbopack: {
+    root: './',
+  }
 };
 
 export default nextConfig;
